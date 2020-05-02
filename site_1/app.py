419b278ccde0ab9af5a2cef97803c52c7cdd0869
eugen <jeny797@gmail.com>
2020-05-02 13:48:22 +0300

    changed templates structure (added base.html and combines with him related files)

--- site_1/app.py
+++ site_1/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import random
 
 from site_data import title, subtitle, description, departures, tours
 
@@ -10,27 +10,25 @@ app = Flask(__name__)
 
 @app.route('/')  # route for main page
 def render_main():
-    return render_template('index.html', title=title, subtitle=subtitle, description=description, departures=departures)
-
-
-@app.route('/departures/')
-def render_departures():
-    return render_template('departure.html')
-
-
-@app.route('/tours/')
-def render_tours():
-    return render_template('departure.html')
+    return render_template('index.html', title=title, subtitle=subtitle, description=description,
+                           departures=departures)
 
 
 @app.route('/departures/<departure>/')
-def render_departure_page(departure):
-    return 'здесь будет направление ' + departure
+def render_departures_page(departure):
+    tours_list = {i:tours[i]  for i in random.sample(list(tours), 3)}
+    return render_template('departure.html', departures=departures, tours_list=tours_list)
 
 
 @app.route('/tours/<int:id>/')
 def render_tour_page(id):
-    #
-    return 'здесь будет тур' + str(id)
+    tour = tours.get(id, None)
+    print(tour)
+    if tour is None:
+        # added the departures key in hope, that will be fix this bug in next course steps
+        return render_template('bad_data.html', departures=departures)
+    else:
+        # added the departures key in hope, that will be fix this bug in next course steps
+        return render_template('tour.html', tour=tour, departures=departures)
 
 app.run('0.0.0.0', 8000, debug=True)
